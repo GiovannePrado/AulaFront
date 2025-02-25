d565b76442dfe5be11fa33ccb46ea8f1cf366d1c
GiovannePrado <giojaum@gmail.com>
2025-02-24 22:21:49 -0300

    Calculadora

--- aula-vite/src/pages/PaginaAleatoria.tsx
+++ aula-vite/src/pages/PaginaAleatoria.tsx
@@ -1,7 +1,56 @@
-function PaginaAleatoria () {
+import { useState } from "react";
+
+function PaginaAleatoria(){
+
+    // const [numero, setnumero] = useState(0);
+    
+    const [numeroUm, setnumeroUm] = useState (0)
+    function ChangeNumeroUm(Event : React.ChangeEvent<HTMLInputElement>) {
+        setnumeroUm(Number(Event.target.value));
+    }
+
+
+    const [numeroDois, setnumeroDois] = useState (0)
+    function ChangeNumeroDois(Event : React.ChangeEvent<HTMLInputElement>) {
+        setnumeroDois(Number(Event.target.value));
+    }
+
+    const [Resultado, setResultado] = useState (0)
+
+    function handleClickButtonMais(){
+        setResultado(numeroUm + numeroDois);
+    }
+
+    function handleClickButtonMenos(){
+        setResultado(numeroUm - numeroDois);
+    }
+
+    function handleClickButtonX(){
+        setResultado(numeroUm * numeroDois);
+    }
+
+    function handleClickButtonDiv(){
+        setResultado(numeroUm / numeroDois);
+    }
+
     return(
-        <div>aleatoria</div>
+        <div>
+            <h1>Calculadora</h1>
+            <br />
+            <input type="Numero" onChange={ChangeNumeroUm} />
+            <input type="Numero" onChange={ChangeNumeroDois} />
+            <br />
+            <button onClick={handleClickButtonMais}> Somar </button>
+            <button onClick={handleClickButtonMenos}> Subtrair </button>
+            <button onClick={handleClickButtonX}> Multiplicar </button>
+            <button onClick={handleClickButtonDiv}> Dividir </button>
+            <br />
+            {Resultado} 
+        </div>
     )
+
+
 }
+
 
 export default PaginaAleatoria
